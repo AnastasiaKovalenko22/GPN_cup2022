90d1a6a7d8a0c9afb33c7eee1c80f23fa2d057e7
Anastasia Kovalenko <kovalnko.nk-22@yandex.ru>
2022-11-04 13:38:06 +0500

    first commit

---
+++ src/main/java/gpn/cup/vk_case/VkCaseApplication.java
@@ -0,0 +1,13 @@
+package gpn.cup.vk_case;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VkCaseApplication.class, args);
+    }
+
+}
