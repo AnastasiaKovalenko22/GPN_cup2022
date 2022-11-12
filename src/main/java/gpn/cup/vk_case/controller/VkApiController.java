17f81d1a3c2e83c3a1df7ac00f454b19c4b0d82d
Anastasia Kovalenko <kovalnko.nk-22@yandex.ru>
2022-11-12 21:42:28 +0500

    добавила кэш

--- src/main/java/gpn/cup/vk_case/controller/VkApiController.java
+++ src/main/java/gpn/cup/vk_case/controller/VkApiController.java
@@ -1,5 +1,8 @@
 package gpn.cup.vk_case.controller;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import gpn.cup.vk_case.dto.RequestMembershipDto;
 import gpn.cup.vk_case.dto.VkResponseDto;
 import gpn.cup.vk_case.exception.NoUserException;
@@ -10,10 +13,35 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class VkApiController {
     private final VkApiService vkApiService;
+    private final LoadingCache<String, VkResponseDto> cache =
+            CacheBuilder.newBuilder().expireAfterWrite(1, TimeUnit.MINUTES)
+                    .maximumSize(100)
+                    .concurrencyLevel(10)
+                    .build(new CacheLoader<>() {
+                        @Override
+                        public VkResponseDto load(String request) throws ExecutionException {
+                            try {
+                                String[] params = request.split(" ");
+                                String userId = params[0];
+                                String groupId = params[1];
+                                String token = params[2];
+                                Map<String, String> firstLastAndMiddleName =
+                                        vkApiService.getFirstLastAndMiddleNameFromVk(userId, token);
+                                Boolean isMember = vkApiService.vkApiIsMember(userId, groupId, token);
+                                return new VkResponseDto(firstLastAndMiddleName.get("last_name"),
+                                        firstLastAndMiddleName.get("first_name"),
+                                        firstLastAndMiddleName.get("middle_name"), isMember);
+                            }catch(VkApiException | NoUserException e){
+                                throw new ExecutionException(e);
+                            }
+                        }
+                    });
 
     public VkApiController(VkApiService vkApiService) {
         this.vkApiService = vkApiService;
@@ -21,18 +49,18 @@ public class VkApiController {
 
     @GetMapping("isMember")
     public ResponseEntity<Object> isMember(@RequestHeader(name = "vk_service_token") String vkServiceToken,
-                                           @Valid @RequestBody RequestMembershipDto request){
+                                           @Valid @RequestBody RequestMembershipDto request) {
         try {
-            Map<String, String> firstLastAndMiddleName =
-                    vkApiService.getFirstLastAndMiddleNameFromVk(request.getUserId(), vkServiceToken);
-            Boolean isMember = vkApiService.vkApiIsMember(request.getUserId(), request.getGroupId(), vkServiceToken);
-            return new ResponseEntity<>(new VkResponseDto(firstLastAndMiddleName.get("last_name"),
-                    firstLastAndMiddleName.get("first_name"),
-                    firstLastAndMiddleName.get("middle_name"), isMember), HttpStatus.OK);
-        }catch (VkApiException e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }catch (NoUserException e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+            VkResponseDto result = cache.get(String.format("%s %s %s",
+                    request.getUserId(), request.getGroupId(), vkServiceToken));
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (ExecutionException e) {
+            String message = e.getCause().getMessage();
+            message = message.substring(message.indexOf(":") + 1);
+            if(message.equals("User not found")){
+                return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
